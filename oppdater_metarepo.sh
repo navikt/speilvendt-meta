acb95627a66c2c82405d41b576b7956da403fcc9
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2026-01-27 09:40:47 +0100

    🏗️ Oppdater metarepo basert på speilvendt-bømlo.txt heller enn egen fil

--- oppdater_metarepo.sh
+++ oppdater_metarepo.sh
@@ -1,8 +1,8 @@
 meta init --force
-while read -r project url
+while read -r project
 do
-    meta project import "$project" "git@github.com:navikt/$project".git
-done < repos.txt
+    meta project import "${project#"navikt/"}" "git@github.com:$project".git
+done < tbd/repositories/speilvendt_bomlo.txt
 
 # Fjern duplikater fra .gitignore
 echo "$(sort -u .gitignore | cat -n | sort -nk1 | cut -f2-)" > .gitignore
